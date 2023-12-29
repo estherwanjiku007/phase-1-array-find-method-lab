b4e7b705a95ef49aa5f6727430e5a4a29dc22b7f
ESTHER WANJIKU <ESTHERWANJIQU@GMAIL.COM>
2023-12-29 17:25:18 +0300

    aster

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const record = []    
+function superBowlWin(collection){
+    const record2=record.find((result)=>result==="W"{
+        return "collection.year":"undeifned"
+    }
+
+//for (const record of collection){
+  //  if(result==="W"){
+    //    console.log(record.year);
+    //}
+    //else {
+       // console.log("undefined");
+   // }
+//}
+//}
+//superBowlWin(record)
+//console.log(record.find(superBowlWin));
